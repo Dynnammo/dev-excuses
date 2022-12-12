75da85d45a5b095f73cb9e27f41bce83d0b0f855
Dynnammo <baptiste.thivend@protonmail.com>
2022-12-12 22:54:30 +0100

    Add minimal setup to return random english excuse

--- src/dev-excuses.cr
+++ src/dev-excuses.cr
@@ -1,10 +1,17 @@
 require "kemal"
+require "yaml"
 
 module Dev::Excuses
   VERSION = "0.1.0"
+  
+  data = File.open("src/data/excuses.yml") do |excuses|
+    YAML.parse(excuses)["data"]
+  end
 
   get "/" do
-    "Hello, world !"
+    key = data.as_h.keys[Random.rand(data.size)]
+    
+    data[key].to_s
   end
 
 end
